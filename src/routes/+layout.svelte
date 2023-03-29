57620aa837418ed004cc9c899a4362d5c5f30e62
Yahya Kisana <ykisana@protonmail.com>
2023-03-29 11:03:23 -0600

    UI - Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,6 +24,12 @@
 	<title>PolyglotAI</title>
 </svelte:head>
 
+<div class="navbar bg-base-100">
+	<div class="navbar-start" />
+	<div class="navbar-center">
+		<a class="btn btn-ghost normal-case text-xl">PolyglotAI</a>
+	</div>
+</div>
 <main>
 	<SideBarView><slot /></SideBarView>
 	<MobileView />
